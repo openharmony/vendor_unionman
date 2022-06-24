f203a20777bfaa2f0b0a0c59b55d8bb889729dc2
kai.li <kai.li@unionman.com.cn>
2022-06-24 20:54:22 +0800

    提交版权和许可证
    Signed-off-by: kai.li <kai.li@unionman.com.cn>

--- a311d/drivers/media/drivers/stream_input/parser/dvb_ci/cimax/dvb_ca_en50221_cimax.h
+++ a311d/drivers/media/drivers/stream_input/parser/dvb_ci/cimax/dvb_ca_en50221_cimax.h
@@ -1,17 +1,24 @@
 /*
- * Copyright (c) 2022 Unionman Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * dvb_ca.h: generic DVB functions for EN50221 CAM CIMAX interfaces
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Parts of this file were based on sources as follows:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * based on code:
+ *
+ * Copyright (C) 1999-2002 Ralph  Metzler
+ *                       & Marcus Metzler for convergence integrated media GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
+
 
 #ifndef _DVB_CA_EN50221_CIMAX_H_
 #define _DVB_CA_EN50221_CIMAX_H_
--- a311d/drivers/rtl88x2cs/core/crypto/rtw_crypto_wrap.h
+++ a311d/drivers/rtl88x2cs/core/crypto/rtw_crypto_wrap.h
@@ -1,16 +1,8 @@
 /*
- * Copyright (c) 2022 Unionman Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2012, Jouni Malinen <j@w1.fi>
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
  */
 
 #ifndef RTW_CRYTO_WRAP_H
