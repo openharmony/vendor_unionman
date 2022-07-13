403fbaa81b1ad012aeb0e532184c814cccb76b20
kai.li <kai.li@unionman.com.cn>
2022-07-13 12:03:12 +0800

    修改九联unionpi-tiger开发板外设开发样例集can模块规范
    Signed-off-by: kai.li <kai.li@unionman.com.cn>

--- unionpi_tiger/sample/hardware/can/canconfig.c
+++ unionpi_tiger/sample/hardware/can/canconfig.c
@@ -349,8 +349,10 @@ static inline void print_ctrlmode(__u32 cm_flags)
     (void)fprintf(stdout,
                   "loopback[%s], listen-only[%s], tripple-sampling[%s],"
                   "one-shot[%s], berr-reporting[%s]\n",
-                  (cm_flags & CAN_CTRLMODE_LOOPBACK) ? "ON" : "OFF", (cm_flags & CAN_CTRLMODE_LISTENONLY) ? "ON" : "OFF",
-                  (cm_flags & CAN_CTRLMODE_3_SAMPLES) ? "ON" : "OFF", (cm_flags & CAN_CTRLMODE_ONE_SHOT) ? "ON" : "OFF",
+                  (cm_flags & CAN_CTRLMODE_LOOPBACK) ? "ON" : "OFF",
+                  (cm_flags & CAN_CTRLMODE_LISTENONLY) ? "ON" : "OFF",
+                  (cm_flags & CAN_CTRLMODE_3_SAMPLES) ? "ON" : "OFF",
+                  (cm_flags & CAN_CTRLMODE_ONE_SHOT) ? "ON" : "OFF",
                   (cm_flags & CAN_CTRLMODE_BERR_REPORTING) ? "ON" : "OFF");
 }
 
