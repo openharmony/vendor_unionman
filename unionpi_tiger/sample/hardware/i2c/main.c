22f43dd06f2e92a18d75de8b9939837492e58279
jiadong.wu <jiadong.wu@unionman.com.cn>
2022-08-11 15:25:28 +0800

    add i2c sample
    Signed-off-by: jiadong.wu <jiadong.wu@unionman.com.cn>

--- unionpi_tiger/sample/hardware/i2c/main.c
+++ unionpi_tiger/sample/hardware/i2c/main.c
@@ -1,86 +1,81 @@
 /*
-* Copyright (c) 2022 Unionman Technology Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2022 Unionman Technology Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
-#include "um_i2c.h"
+#include <unistd.h>
+#include <stdio.h>
+
+#include "securec.h"
+#include "hilog/log.h"
+
+#include "i2cinterface.h"
+
+#define DEVICE_ADDR 0x45
 
 void user_help(void)
 {
-    printf("Please input like this: deceivei2c <int> [int] \n");
-    printf("\t\tthere must have one parameter it is int for range[0,1] \n");
-    printf("\t\tthe second is to write the id to the eeprom range[1,254] \n");
+    printf("Please input like this: i2c_test <int> <int> \n");
+    printf("\t\tthe first is to choice the mps range[0,4] \n");
+    printf("\t\tthe second is to choice the repeatability range[0,2] \n");
 }
 
 int main(int argc, char *argv[])
 {
+    char *dev_name = "/dev/i2c-5";
+    int result = -1;
+    uint8_t mps = 0;
+    uint8_t repeatability = 0;
+
+    sStatusReg_t regRaw;
+    sRHAndTemp_t tempRH = {0};
+
     if (argc < 2L || argc > 3L) {
         user_help();
         return 0;
     }
 
-    if ((strcmp(argv[1], "1") != 0) && (strcmp(argv[1], "0") != 0)) {
-        printf("Invalid first param \n");
+    mps = atoi(argv[1]);
+    repeatability = atoi(argv[2L]);
+    if (mps < 0 || mps > 4L || repeatability < 0 || repeatability > 2L) {
         user_help();
         return 0;
     }
 
-    int number = 0;
-    if (argc == 3L) {
-        number = atoi(argv[2L]);
-        if (number > 254L || argv[2L][0] < 48L || argv[2L][0] > 57L) {
-            printf("Invalid second param \n");
-            user_help();
-            return 0;
-        }
+    SoftReset(dev_name, DEVICE_ADDR);
+    usleep(50L * 1000L); // 等待重启完成
+
+    // 这里仅为测试读取状态寄存器是否正常，具体应用需用户进行编写
+    result = ReadStatusReg(dev_name, DEVICE_ADDR, &regRaw);
+    if (result != 0) {
+        HILOG_ERROR(LOG_CORE, "ReadStatusRegister fail! \n");
+        return result;
+    }
+    usleep(50L * 1000L); // 等待读取寄存器完成
+
+    ModeSet(dev_name, DEVICE_ADDR, mps, repeatability);
+    usleep(50L * 1000L); // 等待更改模式完成
+
+    result = ReadTempAndHum(dev_name, DEVICE_ADDR, &tempRH);
+    if (result != 0) {
+        HILOG_ERROR(LOG_CORE, "ReadTemperatureAndHumidity fail! \n");
+        return result;
     }
 
-    int ret = 0;
-    int i2cChannel = I2C_DEV_3;
-
-    unsigned char tem[2] = {0x01};
-    if (strcmp(argv[1], "1") == 0) {
-        ret = i2c_write(i2cChannel, 0x53, 0xf0, tem, 1);
-        if (ret != 0) {
-            printf("write flag error \n");
-            return 0;
-        } else {
-            printf("write flag success \n");
-        }
-    } else if (strcmp(argv[1], "0") == 0) {
-        tem[0] = 0xff;
-        ret = i2c_write(i2cChannel, 0x53, 0xf0, tem, 1);
-        if (ret != 0) {
-            printf("remove flag error \n");
-            return 0;
-        } else {
-            printf("remove flag success \n");
-        }
-    }
-
-    if (argc == 3L) {
-        tem[0] = number;
-        ret = i2c_write(i2cChannel, 0x53, 0xf1, tem, 1);
-        if (ret != 0) {
-            printf("write backversion error \n");
-            return 0;
-        } else {
-            printf("write backversion success \n");
-        }
-    }
+    printf(" TemperatureC: %f °C \n Humidity: %f %%RH \n TemperatureF: %f °F \n", tempRH.TemperatureC, tempRH.Humidity,
+           tempRH.TemperatureF);
 
     return 0;
 }
