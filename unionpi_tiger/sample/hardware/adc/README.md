c1861d7d750afc6b3fb4c11170e1328e79191550
siyuan.liu <siyuan.liu@unionman.com.cn>
2022-08-12 10:55:09 +0800

    update sample adc
    Signed-off-by: siyuan.liu <siyuan.liu@unionman.com.cn>

--- unionpi_tiger/sample/hardware/adc/README.md
+++ unionpi_tiger/sample/hardware/adc/README.md
@@ -1,28 +1,36 @@
-# ADC读取demo
+# 读取ADC并转化为温度demo
 
 ## ADC参数
 
-参考sample/adc目录：
+详见`um_adc.h`：
 
-```java
+```c
 #define ADC_1	2
 #define ADC_2	3
-#define ADC_HW	1
-#define ADC_HP	0
-#define ADC_CHANNEL_1	"/sys/bus/iio/devices/iio:device0/in_voltage2_input"
-#define ADC_CHANNEL_2	"/sys/bus/iio/devices/iio:device0/in_voltage3_input"
-#define ADC_CHANNEL_HW	"/sys/bus/iio/devices/iio:device0/in_voltage1_input"
-#define ADC_CHANNEL_HP	"/sys/bus/iio/devices/iio:device0/in_voltage0_input"
+#define ADC_CHANNEL_1	"/sys/bus/iio/devices/iio:device0/in_voltage2_raw"
+#define ADC_CHANNEL_2	"/sys/bus/iio/devices/iio:device0/in_voltage3_raw"
 ```
 
 ## 获取ADC接口：
 
-```python
+详见`um_adc.c：
+
+```c
 int get_adc_data(int adc_channel, int * value);
 ```
 
-## ADC连接图
+## 外设
 
-图1	adc连接实物图
+[LM35线性模拟温度传感器](https://wiki.dfrobot.com.cn/_SKU_DFR0023_LM35%E7%BA%BF%E6%80%A7%E6%A8%A1%E6%8B%9F%E6%B8%A9%E5%BA%A6%E4%BC%A0%E6%84%9F%E5%99%A8)
+
+![](../figures/adc_DFRobot_LM35.jpg)
+
+## 连线
+
+- 红线 -- 3.3V
+- 黑线 -- GND
+- 蓝线 -- ADC_1
+
+连线实物图
 
 ![](../figures/adc_demo.png)
--- unionpi_tiger/sample/hardware/adc/main.c
+++ unionpi_tiger/sample/hardware/adc/main.c
@@ -1,30 +1,31 @@
 /*
-* Copyright (c) 2022 Unionman Technology Co., Ltd.
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2022 Unionman Technology Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <stdio.h>
-
 #include "um_adc.h"
 
 int main(int argc, char **argv)
 {
-    int adc_channel = ADC_1; // ADC_2
-    int value;
+    int value = 0;
+    double temperature = 0;
 
-    get_adc_data(adc_channel, &value);
-
-    printf("adc:[%d]\n", value);
+    if (get_adc_data(ADC_1, &value) >= 0) {
+        temperature = ((double)value) * TEMP_INDEX;
+        printf("adc:[%d]\n", value);
+        printf("temperature:[%.1f]C\n", temperature);
+    }
 
     return 0;
 }
