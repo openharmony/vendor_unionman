e08dcaf8d746b9a4d58b977bcb28daf70575e4c8
刘家辉 <jiahui.liu@unionman.com.cn>
2023-10-16 11:38:04 +0800

    提交A311D厂测代码
    Signed-off-by: 刘家辉 <jiahui.liu@unionman.com.cn>

---
+++ unionpi_tiger/sample/napi/A311D_FactoryTest/@ohos.spitest.d.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2023 Unionman Technology Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * unionman a311d
+ * @since 8
+ * @devices phone, tablet
+ * @import import keyTest from '@ohos.keyTest';
+ * @permission N/A
+ */
+
+declare namespace spitest {
+    /**
+     * 获取电源键按键状态
+     * @param callback 	回调函数，返回0：蓝牙接口测试不合格；返回1：蓝牙接口测试合格
+     */
+    function UM_spi_test(): Promise<number>;
+}
+
+export default spitest;
