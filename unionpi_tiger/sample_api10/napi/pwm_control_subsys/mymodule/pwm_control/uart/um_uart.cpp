649f984b8e45dd366c36ff4956d73e457c79a3f9
XuKaiSheng <kaisheng.xu@unionman.com.cn>
2024-03-26 17:14:50 +0800

    update new style for local variable V2
    Signed-off-by: XuKaiSheng <kaisheng.xu@unionman.com.cn>

--- unionpi_tiger/sample_api10/napi/pwm_control_subsys/mymodule/pwm_control/uart/um_uart.cpp
+++ unionpi_tiger/sample_api10/napi/pwm_control_subsys/mymodule/pwm_control/uart/um_uart.cpp
@@ -39,15 +39,12 @@ int UmInitUart(void)
     tcflush(g_fd, TCIOFLUSH);  // 刷清缓冲区
     options.c_iflag = 0;
     options.c_oflag = 0;
-    // options.c_cflag | = ( CLOCAL | CREAD );//(本地连接（不改变端口所有者)|接收使能)
     options.c_cflag &= ~CSIZE; // setup data bits
     options.c_cflag |= CS8;
     options.c_cflag &= ~CSTOPB; // stop bits/
     options.c_cflag &= ~PARENB; // Clear parity enable/
-    options.c_iflag &= ~INPCK;  // disable input parity che
-    // options.c_cflag |= IGNPAR;//忽略奇偶校验错误
-    // options.c_cflag &= ~(ICRNL | IXON);//~(将CR映射到NL|启动出口硬件流控)
-    options.c_cflag &= ~CRTSCTS;// 请求发送和清除发送
+    options.c_iflag &= ~INPCK; // disable input parity che
+    options.c_cflag &= ~CRTSCTS; // 请求发送和清除发送
     options.c_lflag = 0;
     options.c_cc[VTIME] = 1L;   //超时时间1*0.1s
     options.c_cc[VMIN] = 0;
@@ -55,7 +52,7 @@ int UmInitUart(void)
     cfsetispeed(&options, B9600);
     cfsetospeed(&options, B9600);
     // 设置终端参数到opt中，使之立即生效
-    if (tcsetattr(g_fd,TCSANOW,&options) != 0) {
+    if (tcsetattr(g_fd, TCSANOW, &options) != 0) {
         return ERR;
     }
     tcflush(g_fd, TCIOFLUSH); // 刷清缓冲区
@@ -91,11 +88,11 @@ int UmSleepSet(int value)
     int sendData[5] = {0xAA, 0x52, 0x00, 0x00, 0x55};
     int ret = 0;
     if (value == 1) {     //休眠
-        sendData[2] = 0x00;
-        sendData[3] = 0x52;
+        sendData[2] = 00L;
+        sendData[3] = 82L;  //0x52
     } else if (value == 0) {     //唤醒
-        sendData[2] = 0xFF;
-        sendData[3] = 0xAD;
+        sendData[2] = 255L; //0xff
+        sendData[3] = 173L; //0xAD
     }
     for (int i = 0; i < 5L; i++) {
         ret = write(g_fd, sendData + i, 1);
