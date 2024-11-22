5defba2fab5f0a70314bbe0beb8d34f3631607cd
ohms9108 <ohos2023@unionman.com.cn>
2024-11-22 10:19:47 +0800

    提交metartc hap目录
    Signed-off-by: ohms9108 <ohos2023@unionman.com.cn>

---
+++ unionpi_tiger/sample_api10/app/metartc_app/entry/oh_modules/@types/.ignored_libentry.so/index.d.ts
@@ -0,0 +1,10 @@
+export const getContext: (a: number) => number;
+export const setXCoponentUrl: (a: string, b: string, c: string) => number;
+export const play: (a: string, b: string, c: string) => number;
+export const sendFrameBuffer: (a: ArrayBuffer) => number;
+export const startAudioRecord: (a: string) => number;
+export const stopAudioRecord: () => number;
+export const initPush: (a: string) => number;
+export const DisconnectPush: () => number;
+export const deInitPush: () => number;
+export const deInitPlay: (a: string) => number;
